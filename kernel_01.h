cc49830d46ac49d013dc52d80e5718594a84601e
munhoz <pedr.munhoz@gmail.com>
2017-06-26 00:37:09 -0300

    First kernel .c and .h files created, .h now contains the necessary includes and defines and the the headers for the basic functions. Headers do not contain parameters or pointers yet

---
+++ kernel_01.h
@@ -0,0 +1,36 @@
+/*
+    Projeto Sistemas Operacionais II
+    Simulacao de um Sistema Operacional multiprogramado no DOS
+    (processador 8086)
+
+    Operational Systems II project
+    Simulating an multi-programmed Operational System to run
+    on the 8086 processor DOS
+
+    Frederico Gomes
+    Luccas Quadros
+    Pedro Munhoz
+*/
+
+// Checks if the "KERNEL_01" token have been defined, if not,
+// runs the following
+#ifndef KERNEL_01
+  #define KERNEL_01
+  // c basic libraries
+  #include <stdio.h>
+  #include <conio.h>
+  #include <system.h>
+
+  // primary functions headers
+  void far system_init(); // initiates the system
+  void far write(); // writes messages
+  void far scheduler(); // process scheduler
+  void far DOS_return(); // gives the control back to DOS
+  void far end_process(); // finishes the process
+  void far clear_queue(); // clear the process array
+  int far round_robin(); // round robin scheduler algorithm
+  int far process_creator(); // initiates a process
+  int far next_process(); // finds the next active process in the queue
+
+
+#endif
